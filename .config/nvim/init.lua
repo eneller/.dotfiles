6072dd9ae70a0e1c4fc7195f3cab1f973e143e84
eneller <erikneller@gmx.de>
2022-05-22 10:48:56 +0200

    add Packer for nvim packages

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,6 @@
 local o = vim.o
 local wo = vim.wo
 local bo = vim.bo
--- Install packer https://github.com/wbthomason/packer.nvim#bootstrapping
 -- GLOBAL SETTINGS
 o.termguicolors = true
 vim.cmd('colorscheme monokai') -- set colorscheme
@@ -29,3 +28,27 @@ wo.signcolumn = 'yes'
 wo.wrap = false
 
 -- KEYBINDS
+--
+-- INSTALL PACKER https://github.com/wbthomason/packer.nvim#bootstrapping
+local vim = vim
+local execute = vim.api.nvim_command
+local fn = vim.fn
+-- ensure that packer is installed
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+    execute 'packadd packer.nvim'
+end
+vim.cmd('packadd packer.nvim')
+local packer = require'packer'
+local util = require'packer.util'
+packer.init({
+  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+})
+--- startup and add configure plugins
+packer.startup(function()
+  local use = use
+  -- add you plugins here like:
+  use 'neovim/nvim-lspconfig'
+  end
+)
