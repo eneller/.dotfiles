aec5058ee0cb6f8d575c0899c9ecd3e2851a657f
eneller <erikneller@gmx.de>
2022-05-24 10:56:08 +0200

    nvim: ftplugin, multi lua file setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,54 +1,30 @@
-local o = vim.o
-local wo = vim.wo
-local bo = vim.bo
--- GLOBAL SETTINGS
-o.termguicolors = true
+require("plugins")
+require("keybinds")
+-- GENERAL SETTINGS
+vim.o.termguicolors = true
 vim.cmd('colorscheme monokai') -- set colorscheme
-o.syntax = 'on'
-o.errorbells = false
-o.smartcase = true
-o.showmode = true
-bo.swapfile = false
-o.backup = false
-o.undodir = vim.fn.stdpath('config') .. '/undodir'
-o.undofile = true
-o.incsearch = true
-o.hidden = true
-o.completeopt='menuone,noinsert,noselect'
-bo.autoindent = true
-bo.smartindent = true
-o.tabstop = 2
-o.softtabstop = 2
-o.shiftwidth = 2
-o.expandtab = true
-wo.number = true
-wo.relativenumber = true
-o.scrolloff = 8
-wo.signcolumn = 'yes'
-wo.wrap = false
+vim.o.syntax = 'on'
+vim.o.errorbells = false
+vim.o.smartcase = true
+vim.o.showmode = true
+vim.o.ignorecase = true -- case-insensitive search
+vim.bo.swapfile = false
+vim.o.backup = false
+vim.o.undodir = vim.fn.stdpath('config') .. '/undodir'
+vim.o.undofile = true
+vim.o.incsearch = true
+vim.o.hidden = true
+vim.o.completeopt='menuone,noinsert,noselect'
+vim.bo.autoindent = true
+vim.bo.smartindent = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.o.scrolloff = 8
+vim.wo.signcolumn = 'yes'
+vim.wo.wrap = false
+vim.opt.cursorline = true
 
--- KEYBINDS
---
--- INSTALL PACKER https://github.com/wbthomason/packer.nvim#bootstrapping
-local vim = vim
-local execute = vim.api.nvim_command
-local fn = vim.fn
--- ensure that packer is installed
-local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-    execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-    execute 'packadd packer.nvim'
-end
-vim.cmd('packadd packer.nvim')
-local packer = require'packer'
-local util = require'packer.util'
-packer.init({
-  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
-})
---- startup and add configure plugins
-packer.startup(function()
-  local use = use
-  -- add you plugins here like:
-  use 'neovim/nvim-lspconfig'
-  end
-)
