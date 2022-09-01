de389f9e4672019bc0c8935a232745bd7e8a7bf2
eneller <erik.neller@gmx.de>
2022-09-01 23:07:17 +0200

    firefox stuff

--- .mozilla/firefox/4vd8uypc.default-release/user.js
+++ .mozilla/firefox/4vd8uypc.default-release/user.js
@@ -5,12 +5,12 @@ user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
 // http://kb.mozillazine.org/Browser.offline-apps.notify
 user_pref("browser.offline-apps.notify", true);
 // PREF: Do not check if Firefox is the default browser
-user_pref("browser.shell.checkDefaultBrowser",			false);
+user_pref("browser.shell.checkDefaultBrowser", false);
 // PREF: Disable the "new tab page" feature and show a blank tab instead
 // https://wiki.mozilla.org/Privacy/Reviews/New_Tab
 // https://support.mozilla.org/en-US/kb/new-tab-page-show-hide-and-customize-top-sites#w_how-do-i-turn-the-new-tab-page-off
 user_pref("browser.newtabpage.enabled",	false);
-user_pref("browser.newtab.url", "https://web.neller.org/dashboard");
+user_pref("browser.newtab.url", "web.neller.org/dashboard");
 // PREF: DNS over HTTPS
 user_pref("network.trr.mode", 2);
 user_pref("network.trr.uri", "https://mozilla.cloudflare-dns.com/dns-query");
@@ -30,3 +30,5 @@ user_pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcu
 user_pref("browser.partnerlink.campaign.topsites","");
 // PREF: dont open suggestions at all when entering URL Bar
 user_pref("browser.urlbar.suggest.topsites", false);
+// PREF: disable firefox pocket
+user_pref("extensions.pocket.enabled", false);
