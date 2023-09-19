a0da43b1ed6e5d9dcdf86ff58b0befd29662c7a7
eneller <erik.neller@gmx.de>
2023-09-19 10:41:07 +0200

    autostart nextcloud, nvim binds

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -8,7 +8,9 @@ function map(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+-- frankly i dont know what the hell im doing
 map("n", "<Enter>", "i<Enter><Esc>k$")
 -- map("n", "<S-Enter>", "d$O<Esc>p^")
-map("n", "<Space>", "i<Space><Esc>")
+-- map("n", "<Space>", "i<Space><Esc>")
+map("n", "<A-Space>", "<Esc>")
 
